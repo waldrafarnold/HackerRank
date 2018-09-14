f1dc191cb0b9ac0ea406fab375cc5c2888fbab1c
ROCLJAWALDRAF\Arnold <awaldraf@touchpointmed.com>
2018-09-14 12:48:06 +0300

    Solve Me First https://www.hackerrank.com/challenges/solve-me-first/problem

---
+++ Algorithms/Warmup/Solve Me First/Solution.cs
@@ -0,0 +1,47 @@
+﻿/*
+https://www.hackerrank.com/challenges/solve-me-first
+
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Function prototype:
+
+int solveMeFirst(int a, int b);
+
+where,
+
+a is the first integer input.
+b is the second integer input
+Return values
+
+sum of the above two integers
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+
+5
+Explanation
+
+The sum of the two integers  and  is computed as: .
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+
+    static int solveMeFirst(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main(String[] args)
+    {
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = solveMeFirst(val1, val2);
+        Console.WriteLine(sum);
+    }
+}
