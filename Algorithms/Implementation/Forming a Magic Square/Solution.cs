d5c91dd33c17a8f21d4d1fdb76f8154f481f0015
ROCLJAWALDRAF\Arnold <awaldraf@touchpointmed.com>
2018-09-14 16:38:48 +0300

    Forming a Magic Square https://www.hackerrank.com/challenges/magic-square-forming

---
+++ Algorithms/Implementation/Forming a Magic Square/Solution.cs
@@ -0,0 +1,117 @@
+﻿/*
+https://www.hackerrank.com/challenges/magic-square-forming
+
+We define a magic square to be an  matrix of distinct positive integers from  to  where the sum of any row, column, or diagonal of length  is always equal to the same number: the magic constant.
+
+You will be given a  matrix  of integers in the inclusive range . We can convert any digit  to any other digit  in the range  at cost of . Given , convert it into a magic square at minimal cost. Print this cost on a new line.
+
+Note: The resulting magic square must contain distinct integers in the inclusive range .
+
+For example, we start with the following matrix :
+
+5 3 4
+1 5 8
+6 4 2
+We can convert it to the following magic square:
+
+8 3 4
+1 5 9
+6 7 2
+This took three replacements at a cost of .
+
+Input Format
+
+Each of the lines contains three space-separated integers of row .
+
+Constraints
+
+Output Format
+
+Print an integer denoting the minimum cost of turning matrix  into a magic square.
+
+Sample Input 0
+
+4 9 2
+3 5 7
+8 1 5
+Sample Output 0
+
+1
+Explanation 0
+
+If we change the bottom right value, , from  to  at a cost of ,  becomes a magic square at the minimum possible cost.
+
+Sample Input 1
+
+4 8 2
+4 5 7
+6 1 6
+Sample Output 1
+
+4
+Explanation 1
+
+Using 0-based indexing, if we make
+
+-> at a cost of 
+-> at a cost of 
+-> at a cost of ,
+then the total cost will be .
+ */
+
+
+using System.IO;
+using System;
+
+class Solution
+{
+
+    // Complete the formingMagicSquare function below.
+    static int formingMagicSquare(int[][] s)
+    {
+        int[,,] possiblePermutations = new int[,,]
+        {
+            {{8, 1, 6}, {3, 5, 7}, {4, 9, 2}},
+            {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+            {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}},
+            {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}},
+            {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+            {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}},
+            {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}},
+            {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}},
+        };
+
+        int result = int.MaxValue;
+        for (int possiblePermutation = 0; possiblePermutation < 8; possiblePermutation++)
+        {
+            int permutationCost = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    permutationCost += Math.Abs(s[i][j] - possiblePermutations[possiblePermutation, i, j]);
+            }
+            result = Math.Min(result, permutationCost);
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int[][] s = new int[3][];
+
+        for (int i = 0; i < 3; i++)
+        {
+            s[i] = Array.ConvertAll(Console.ReadLine().Split(' '), sTemp => Convert.ToInt32(sTemp));
+        }
+
+        int result = formingMagicSquare(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
