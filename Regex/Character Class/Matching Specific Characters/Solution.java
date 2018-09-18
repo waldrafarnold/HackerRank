1f9220c08de31ae2e616c20cd4574f91801bbc4f
ROCLJAWALDRAF\Arnold <awaldraf@touchpointmed.com>
2018-09-18 15:39:13 +0300

    Matching Specific Characters https://www.hackerrank.com/challenges/matching-specific-characters

---
+++ Regex/Character Class/Matching Specific Characters/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[1-3][120][xs0][30Aa][xsu][\\.,]$"); // Use \\ instead of using \ 
+    
+    }
+}
