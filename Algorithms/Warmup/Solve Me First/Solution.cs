f919bd446fcf05668c0f99aa0d199de6fbfd256e
ROCLJAWALDRAF\Arnold <awaldraf@touchpointmed.com>
2018-10-05 16:14:51 +0300

    cleanup comments, left only the link

--- Algorithms/Warmup/Solve Me First/Solution.cs
+++ Algorithms/Warmup/Solve Me First/Solution.cs
@@ -1,30 +1,4 @@
-﻿/*
-https://www.hackerrank.com/challenges/solve-me-first
-
-Complete the function solveMeFirst to compute the sum of two integers.
-
-Function prototype:
-
-int solveMeFirst(int a, int b);
-
-where,
-
-a is the first integer input.
-b is the second integer input
-Return values
-
-sum of the above two integers
-Sample Input
-
-a = 2
-b = 3
-Sample Output
-
-5
-Explanation
-
-The sum of the two integers  and  is computed as: .
- */
+﻿// https://www.hackerrank.com/challenges/solve-me-first
 
 using System;
 using System.Collections.Generic;
