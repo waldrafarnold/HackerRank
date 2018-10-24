080db85a61edd89495a7c80285fa498308a94328
ROCLJAWALDRAF\Arnold <awaldraf@touchpointmed.com>
2018-10-24 16:56:33 +0300

    Sum vs XOR https://www.hackerrank.com/challenges/sum-vs-xor

---
+++ Algorithms/Bit Manipulation/Sum vs XOR/Solution.cs
@@ -0,0 +1,36 @@
+﻿//https://www.hackerrank.com/challenges/sum-vs-xor
+using System.IO;
+using System;
+
+class Solution
+{
+
+    // Complete the sumXor function below.
+    static long sumXor(long n)
+    {
+        long result = 0;
+        //get count of 0 digits from the number transformed into binary
+        while (n != 0)
+        {
+            result += (n % 2 == 0) ? 1 : 0;
+            n /= 2;
+        }
+        result = (long)Math.Pow(2, result);
+        return result;
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+        long result = sumXor(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
