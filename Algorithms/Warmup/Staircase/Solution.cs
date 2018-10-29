ee8f88f5deeeb7843c3a91b5a9aa628b47dcb320
ROCLJAWALDRAF\Arnold <awaldraf@touchpointmed.com>
2018-10-29 14:32:27 +0200

    Staircase - https://www.hackerrank.com/challenges/staircase

---
+++ Algorithms/Warmup/Staircase/Solution.cs
@@ -0,0 +1,28 @@
+﻿//https://www.hackerrank.com/challenges/staircase
+//Staircase
+using System;
+
+class Solution
+{
+
+    // Complete the staircase function below.
+    static void staircase(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(i >= n - j - 1 ? "#" : " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        staircase(n);
+    }
+}
