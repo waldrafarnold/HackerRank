8ecb850f13c689e0283880d995c5da02b138c290
ROCLJAWALDRAF\Arnold <awaldraf@touchpointmed.com>
2018-11-08 10:41:00 +0200

    Birthday Cake Candles https://www.hackerrank.com/challenges/birthday-cake-candles

---
+++ Algorithms/Warmup/Birthday Cake Candles/Solution.cs
@@ -0,0 +1,44 @@
+﻿//https://www.hackerrank.com/challenges/birthday-cake-candles
+//Birthday Cake Candles
+using System.IO;
+using System;
+
+class Solution
+{
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar)
+    {
+        var count = 1;
+        var max = ar[0];
+        for (int i = 1; i < ar.Length; i++)
+        {
+            if (ar[i] > max)
+            {
+                max = ar[i];
+                count = 1;
+            }
+            else if (ar[i] == max)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = birthdayCakeCandles(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
